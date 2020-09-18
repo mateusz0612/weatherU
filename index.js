74ea57e9057a750ecb17bcece429e108be8d179d
mateusz0612 <dennyfacio@gmail.com>
2020-09-18 16:29:23 +0200

    Working app/Have to add more styling

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const weatherImg = document.querySelector('.weather-image');
-const temp = document.querySelector('.tempreture');
+const temp = document.querySelector('.temperature');
 const city = document.querySelector('.city');
 const country = document.querySelector('.country');
 const weatherType = document.querySelector('.weather-type');
-
+const feelsLike = document.querySelector('.feels-like');
+const tempMax = document.querySelector('.temp-max');
+const tempMin = document.querySelector('.temp-min');
 
 const toCelsius = (temp) => {
     return (temp - 273.15).toFixed(0);
@@ -14,11 +16,14 @@ const getWeather = (position) => {
     let long = position.coords.longitude;
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=9473b08a5ce2f2071235d0c9469aff71`).then(response => response.json()).then(data => {
         console.log(data);
-        weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
         temp.textContent = toCelsius(data.main.temp);
         city.textContent = data.name + ',';
         country.textContent = data.sys.country;
         weatherType.textContent = data.weather[0].description;
+        feelsLike.textContent += toCelsius(data.main.feels_like) + '°C';
+        tempMax.textContent += toCelsius(data.main.temp_max) + '°C';
+        tempMin.textContent += toCelsius(data.main.temp_min) + '°C';
     });
 }
 
